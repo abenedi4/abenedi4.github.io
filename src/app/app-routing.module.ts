c78e06ff54ac0fe18531ae0a48ade8713d8b128c
Alexandria Benedict <abenedi4@uncc.edu>
2021-01-15 15:18:14 -0500

    Added all files from local

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+
+const routes: Routes = [
+  { path: '',
+component: HomeComponent},
+{ path: 'resume',
+component: ResumeComponent},
+{ path: 'projects',
+component: ProjectsComponent},
+{ path: 'aboutme',
+component: AboutmeComponent},
+{ path: '**',
+component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
